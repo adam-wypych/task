fd0fa07985cdb75e85eec2b5f86da397b34f835e
adam-wypych <adam.wypych.1986@gmail.com>
2025-09-29 21:04:16 +0200

    Initial test template still failing part to be added - failing in complilation is also red in TDD

---
+++ spacex-repository/src/test/java/org/spacex/repository/SpaceXRepositoryTest.java
@@ -0,0 +1,15 @@
+package org.spacex.repository;
+
+import org.junit.Test;
+
+public class SpaceXRepositoryTest {
+
+	@Test
+	public void givenMissionDoesNotExist__whenMissionIsCreated__operationShouldSuccess() {
+		// given
+		
+		// when
+		
+		// then
+	}
+}
