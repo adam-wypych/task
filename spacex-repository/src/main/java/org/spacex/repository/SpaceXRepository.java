f748f553387d2a8408a900810dfb224a0f51e83e
adam-wypych <adam.wypych.1986@gmail.com>
2025-09-29 21:06:40 +0200

    No test is compiling but still we need to add some failing part in logic

---
+++ spacex-repository/src/main/java/org/spacex/repository/SpaceXRepository.java
@@ -0,0 +1,5 @@
+package org.spacex.repository;
+
+public class SpaceXRepository {
+
+}
--- spacex-repository/src/test/java/org/spacex/repository/SpaceXRepositoryTest.java
+++ spacex-repository/src/test/java/org/spacex/repository/SpaceXRepositoryTest.java
@@ -1,9 +1,17 @@
 package org.spacex.repository;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class SpaceXRepositoryTest {
 
+	private SpaceXRepository sut;
+
+	@Before
+	public void setUp() {
+		this.sut = new SpaceXRepository();
+	}
+	
 	@Test
 	public void givenMissionDoesNotExist__whenMissionIsCreated__operationShouldSuccess() {
 		// given
