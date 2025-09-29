f969cee610cce23d632f911b6238a56a6a827bc9
adam-wypych <adam.wypych.1986@gmail.com>
2025-09-29 21:59:32 +0200

    Fixing test failing due to duplicate rockets

--- spacex-repository/src/main/java/org/spacex/repository/SpaceXRepository.java
+++ spacex-repository/src/main/java/org/spacex/repository/SpaceXRepository.java
@@ -3,15 +3,16 @@ package org.spacex.repository;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.spacex.repository.mission.exceptions.DuplicateMissionException;
-
 import org.spacex.model.DefaultMission;
 import org.spacex.model.DefaultRocket;
 import org.spacex.model.Mission;
 import org.spacex.model.Rocket;
+import org.spacex.repository.mission.exceptions.DuplicateMissionException;
+import org.spacex.repository.rocket.exceptions.DuplicateRocketException;
 
 public class SpaceXRepository {
 	private final List<Mission> missions = new ArrayList<>();
+	private final List<Rocket> rockets = new ArrayList<>();
 	
 	public Mission createNewMission(final String missionName) {
 		if (missions.stream().anyMatch(m -> m.getName().equals(missionName))) {
@@ -24,6 +25,13 @@ public class SpaceXRepository {
 	}
 
 	public Rocket createNewRocket(final String rocketName) {
-		return new DefaultRocket(rocketName);
+		if (rockets.stream().anyMatch(r -> r.getName().equals(rocketName))) {
+			throw new DuplicateRocketException(rocketName);
+		}
+		
+		Rocket newRocket = new DefaultRocket(rocketName);
+		rockets.add(newRocket);
+		
+		return newRocket;
 	}
 }
